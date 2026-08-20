7e1af9f58c0532d8c7073f1e32de686e60549c31
Abinaya M <mahendranabinaya14@gmail.com>
2026-08-20 10:58:59 +0530

    Remove nested locked folders

--- Open-Space-Parking-main/lib/core/common/exceptions/network_exception.dart
+++
@@ -1,5 +0,0 @@
-import 'package:open_space_parking/core/common/exceptions/app_exception.dart';
-
-class NetworkException extends AppException {
-  const NetworkException(super.message, {super.code});
-}
